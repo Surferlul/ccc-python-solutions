3cb9083e71f1f1a4e51f256a41646bbb7c9b658c
Lukas Baumann <birnensalz@gmail.com>
2021-02-18 18:36:11 +0100

    Create 6 - The Name Game.py

---
+++ 07/a/6 - The Name Game.py
@@ -0,0 +1,7 @@
+name = input()
+print(
+      name + ", " + name + ", bo-b" + name[1:] + "\n" +
+      "banana-fana fo-f" + name[1:] + "\n" +
+      "fee-fi-mo-m" + name[1:] + "\n" +
+      name + "!"
+      )
